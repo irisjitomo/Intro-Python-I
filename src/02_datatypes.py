cd99eac2e93c66d3144d611daf3c0c97c8127cbd
unknown <irisjitomo@yahoo.com>
2020-01-13 15:23:48 -0500

    03 done

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,7 +14,7 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-print( "problem 1:", x * int(y))
+print( "problem 1:", x + int(y))
 
 
 # Write a print statement that combines x + y into the string value 57
--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,24 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+print(sys.argv)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print('Windows 10')
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print('Python 3.8.1')
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print(("Current Process ID:").upper() ,os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+directory = "current directory: "
+print(directory.upper(), os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print(("Machine login name:").upper(), os.getlogin())
